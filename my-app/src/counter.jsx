fbc60465852f77bc69617a9ca1dbff4f239f4f27
AVASH-github <148526107+AVASH-github@users.noreply.github.com>
2024-12-05 09:21:56 +0000

    Corrected Counter

--- my-app/src/counter.jsx
+++ my-app/src/counter.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
+
 function Counter(){
 
-    const[count,setCount]=useState(0);
+        const[count,setCount]=useState(0);
 
     const clickCount=()=>{
-        setCount(count+1);
+       setCount(count+1);
     }
-    return (
-
+    return(
 <>
-<h1>Counter</h1>
+<h1>COUNTER</h1>
+<button className="add-button" onClick={clickCount}>CLick Me</button>
 
-<button onClick={clickCount} > CLICK ME</button>
+<p>Click Count :{count}</p>
 
-<p>CLICK COUNT: {count}</p>
 </>
 
     );
